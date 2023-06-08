050548433c47bc1eb1bb8e4584d5fce28d9f36b1
Numonjon Turdimurodov <turdimurodovnumonjon@gmail.com>
2023-06-08 19:26:51 +0500

    Two sums problem were solved

--- Easy_problems/1. Two Sum/Program.cs
+++ Easy_problems/1. Two Sum/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace _1._Two_Sum
 {
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int res = 0;
+            for(int i = 0; i < nums.Length; i++) 
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    res = nums[i] + nums[j];
+                    if (res == target)
+                        return new int[2] { i, j};
+                }
+            return new int[2] {0, 0};
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Solution solution = new Solution();
+            var pr = solution.TwoSum(new int[] { 3, 2, 3 }, 6);
+            foreach(int i in pr)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
