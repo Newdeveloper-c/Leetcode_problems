004bf9bc2005c59d141b6014a46d1c15e3417892
Numonjon Turdimurodov <turdimurodovnumonjon@gmail.com>
2023-06-09 19:04:02 +0500

    744. Find Smallest Letter Greater Than Target problem solved

---
+++ Easy_problems/744. Find Smallest Letter Greater Than Target/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _744._Find_Smallest_Letter_Greater_Than_Target
+{
+    public class Solution
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            char ans = letters[0];
+            bool isFirstTime = true;
+            foreach (char c in letters)
+            {
+                if(c > target)
+                if(isFirstTime)
+                {
+                        ans = c;
+                        isFirstTime = false;
+                }                   
+                ans = (ans < c)? ans : c;
+            }
+            return ans;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.NextGreatestLetter(new char[] { 'c', 'f', 'j' }, 'a'));
+        }
+    }
+}
